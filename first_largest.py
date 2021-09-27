f8c57057cc072c8f0c64f194d0435cfbb946cc1c
Rene Santillana <rfsantill00@outlook.com>
2021-09-26 23:45:39 -0500

    Added try, except TypeError, and except for any other case into first_largest.py. Modified test.py to test for a TypeError

--- first_largest.py
+++ first_largest.py
@@ -1,3 +1,4 @@
+import sys
 """
 Python program to find the largest element and its location.
 """
@@ -5,20 +6,26 @@ Python program to find the largest element and its location.
 def largest_element(a, loc):
     """ Return the largest element of a sequence a.
     """
-    largest = a[0]
-    location = 0
-    for i in range(0,len(a)):
+    try:
+        largest = a[0]
+        location = 0
+        for i in range(0,len(a)):
 
-        if largest <= a[i]:
-            largest = a[i]
-            if loc == True:
-                location = i
+            if largest <= a[i]:
+                largest = a[i]
+                if loc == True:
+                    location = i
 
-    if loc == True:
-        return largest, location
-    else:
-        return largest
-    
+        if loc == True:
+            return largest, location
+        else:
+            return largest
+    except TypeError:
+        print("Could not convert data to an integer.")
+        return -1
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -16,6 +16,12 @@ class FirstLargest(unittest.TestCase):
         got = largest_element(given, loc = True)
         self.assertEqual(got, expect)
 
+    def test_largest_is_first_with_string(self):
+        given = [5,3,'s',2,'ksu']
+        expect = -1
+        got = largest_element(given, loc = True)
+        self.assertEqual(got, expect)
+
     """ ADD MORE  """
 
 if __name__ == '__main__':
